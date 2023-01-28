ce1dba4898105da1c2a54be21a3951d334bef988
Braden Orlosky <bradenorlosky@outlook.com>
2023-01-28 17:48:51 -0500

    trouble with importing

---
+++ script.js
@@ -0,0 +1,33 @@
+import java.lang.Math;
+
+let rock = 'rock'
+let paper = 'paper'
+let scissors = 'scissors'
+
+let num = Math.floor(Math.random() * 3);
+
+function compchoice(choice) {
+    if (num == 0) {
+        return rock
+    }
+    else if (num == 1) {
+        return paper
+    }
+    else if (num == 2) {
+        return scissors
+    }
+    else {
+
+    }
+}
+
+yours = prompt('Choose \'rock\', \'paper\', or \'scissors\'')
+
+function your_choice(pick) {
+if (yours == 'rock') {
+    let yours = 0
+}
+}
+
+console.log('Computer\'s pick was', compchoice(num));
+console.log('Your choice was', )
