57ea9e3e0bdcef53b2f4d272bd63039155fe46d3
Braden Orlosky <bradenorlosky@outlook.com>
2023-01-28 18:32:20 -0500

    finished

--- script.js
+++ script.js
@@ -18,21 +18,46 @@ function compchoice(choice) {
 
     }
 }
-
-
-
-function your_choice(pick) {
-yours = prompt('Choose \'rock\', \'paper\', or \'scissors\'')
-if (yours == 'rock') {  
-    return 'rock'
-}
-else if (yours == 'paper') {  
-    return 'paper'
-}
-else if (yours == 'scissors') {
-    return 'scissors'
-}
-}
-
 console.log('Computer\'s pick was', compchoice(num));
-console.log('Your choice was', your_choice(yours))
+
+let my_pick = prompt('rock, paper, or scissors');
+
+function player_game(pick) {
+    let n = 0
+    while (n == 0) {
+    if (my_pick == 'rock') {
+        return rock
+    }
+    else if (my_pick == 'paper') {
+        return paper
+    }
+    else if (my_pick == 'scissors') {
+        return scissors
+    }
+    else {
+        return 'retype your answer'
+    }
+}
+}
+
+console.log('Your pick was', player_game())
+let score_comp = 0
+let score_player = 0
+function winner() {
+    if ((num == 0 && my_pick == 'rock') || (num == 1 && my_pick == 'paper') || (num == 2 && my_pick == 'scissors')){
+        return 'Tie, try again!'
+    }
+    else if ((num == 0 && my_pick == 'paper') || (num == 1 && my_pick == 'scissors') || (num == 2 && my_pick == 'rock')){
+        return 'You win!'
+        let score_player =+ 1
+    }
+    else if ((num == 0 && my_pick == 'scissors') || (num == 1 && my_pick == 'rock') || (num == 2 && my_pick == 'paper')) {
+        return 'You lose!'
+        let score_comp =+ 1
+    }
+    else {
+
+    }
+}
+
+console.log(winner())
